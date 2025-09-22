8dd8cdd7f6fbd7e7d3d671117f27ec29e6ec4c60
Ahmed Fraj <ahmedfraj@web.de>
2025-09-22 18:09:44 +0200

    Responsive breakpoints, Smooth animations (like slide or fade effects), Custom colors (e.g., for navigation buttons or pagination)

--- script.js
+++ script.js
@@ -12,10 +12,13 @@ menuCloseButton.addEventListener("click", () => menuOpenButton.click());
 //Initialize Swiper
 const swiper = new Swiper('.slider-wrapper', {
   loop: true,
+  spaceBetween: 25,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
   },
 
   // Navigation arrows
@@ -23,5 +26,17 @@ const swiper = new Swiper('.slider-wrapper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  },
+
+  //Resposive breakpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView:2
+    },
+    1024: {
+        slidesPerView: 3
+    }
+  }
 });
