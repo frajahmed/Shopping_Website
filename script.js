4c902169331a3adea1cf0dc2a0c6600899f68bf5
Ahmed Fraj <ahmedfraj@web.de>
2025-09-22 17:33:11 +0200

    Import Swiper into This project for the Testimonials section (comments the condom) with Used Swiper from CDN

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ menuOpenButton.addEventListener("click", () => {
 
 //Close menu when the closee is clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+//Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  },
+});
